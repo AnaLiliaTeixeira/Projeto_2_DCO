2fb200b3b3a515536b30def9ad8852550fd8c9bf
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-04 18:06:01 +0100

    Criação da classe Item

---
+++ src/migrantmatcher/domain/Item.java
@@ -0,0 +1,14 @@
+package migrantmatcher.domain;
+
+public class Item {
+
+	private String descricao;
+	
+	public Item(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+}
