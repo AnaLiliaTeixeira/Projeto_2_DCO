54e903cf18efa0dfada52de74141ebcbec723dfc
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-04 21:43:21 +0100

    Método pedeListaRegioesPossiveis acabado

--- src/migrantmatcher/controllers/ProcuraAjudaHandler.java
+++ src/migrantmatcher/controllers/ProcuraAjudaHandler.java
@@ -1,13 +1,18 @@
 package migrantmatcher.controllers;
 
+import java.util.List;
+
+import migrantmatcher.domain.Ajuda;
 import migrantmatcher.domain.CatalogoMigrantes;
 import migrantmatcher.domain.ListaAjudas;
 import migrantmatcher.domain.ListaRegioes;
 import migrantmatcher.domain.Migrante;
+import migrantmatcher.domain.Regiao;
 
 public class ProcuraAjudaHandler {
 
 	private CatalogoMigrantes cm = new CatalogoMigrantes();
+	private ListaRegioes lr = new ListaRegioes();
 
 	public void registaMigrante(String nome, int contacto) {
 		cm.addMigrante(new Migrante(nome, contacto));		
@@ -18,12 +23,17 @@ public class ProcuraAjudaHandler {
 		
 	}
 
-	public ListaRegioes pedeListaRegioesPossiveis() {
-		// TODO Auto-generated method stub
+	public List<Regiao> pedeListaRegioesPossiveis() {
+		return lr.getListaRegioes();
 		
 	}
 
 	public ListaAjudas indicaRegiaoEscolhida(String regiao) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public Ajuda escolheAjuda(String ajuda) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -32,5 +42,6 @@ public class ProcuraAjudaHandler {
 		// TODO Auto-generated method stub
 		
 	}
+
 	
 }
