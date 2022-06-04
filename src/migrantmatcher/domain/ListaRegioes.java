6f8061e91d849f40dc1411a94538fd566c8bc925
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-04 18:06:14 +0100

    Criação da classe ListaRegioes

---
+++ src/migrantmatcher/domain/ListaRegioes.java
@@ -0,0 +1,23 @@
+package migrantmatcher.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaRegioes {
+
+	private List<Regiao> lr = new ArrayList<>();
+	String[] nutsII = {"NORTE", "ALGARVE", "CENTRO", "ÁREA_METROPOLITANA_DE_LISBOA",
+			"ALENTEJO", "REGIÃO_AUTÓNOMA_DOS_AÇORES", "REGIÃO_AUTÓNOMA_DA_MADEIRA"};
+	
+	
+	public List<Regiao> getListaRegioes() {
+		for (String s : nutsII) {
+			lr.add(setRegiao(s));
+		}
+		return lr;
+	}
+	
+	public Regiao setRegiao(String regiao) {
+		return new Regiao(regiao);
+	}
+}
