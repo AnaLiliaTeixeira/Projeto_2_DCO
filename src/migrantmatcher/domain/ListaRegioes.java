1702cb327b7f597efaa8076198812840ea4731c5
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-04 21:42:12 +0100

    Visibilidade da variável nutsII tornada private

--- src/migrantmatcher/domain/ListaRegioes.java
+++ src/migrantmatcher/domain/ListaRegioes.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ListaRegioes {
 
 	private List<Regiao> lr = new ArrayList<>();
-	String[] nutsII = {"NORTE", "ALGARVE", "CENTRO", "ÁREA_METROPOLITANA_DE_LISBOA",
+	private String[] nutsII = {"NORTE", "ALGARVE", "CENTRO", "ÁREA_METROPOLITANA_DE_LISBOA",
 			"ALENTEJO", "REGIÃO_AUTÓNOMA_DOS_AÇORES", "REGIÃO_AUTÓNOMA_DA_MADEIRA"};
 	
 	
