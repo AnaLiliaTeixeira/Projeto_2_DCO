7e6b1f94d7d3457558c11ca9f080818f43ecd64c
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-04 18:07:05 +0100

    Criação da classe Regiao

---
+++ src/migrantmatcher/domain/Regiao.java
@@ -0,0 +1,13 @@
+package migrantmatcher.domain;
+
+public class Regiao {
+	
+	private String nome;
+	
+	public Regiao(String regiao) {
+	}
+
+	public String getRegiao() {
+		return this.nome;
+	}
+}
