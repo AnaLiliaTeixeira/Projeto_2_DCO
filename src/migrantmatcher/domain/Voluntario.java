10873e4649e1a8ba3da164a6632aff22bd1b8a89
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-04 18:07:30 +0100

    Criação da classe Voluntario

---
+++ src/migrantmatcher/domain/Voluntario.java
@@ -0,0 +1,14 @@
+package migrantmatcher.domain;
+
+public class Voluntario {
+
+	private int contacto;
+	
+	public Voluntario(int contacto) {
+		this.contacto = contacto;
+	}
+	
+	public int getContacto() {
+		return contacto;
+	}
+}
