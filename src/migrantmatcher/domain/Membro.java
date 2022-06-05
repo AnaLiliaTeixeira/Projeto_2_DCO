11b5b7d3870b01e94a15349cf7ef654f439e31e0
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-05 12:16:25 +0100

    Criação da classe Membro

---
+++ src/migrantmatcher/domain/Membro.java
@@ -0,0 +1,14 @@
+package migrantmatcher.domain;
+
+public class Membro {
+
+	private String nome;
+	
+	public Membro(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+}
