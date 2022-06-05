2eb821b850827049087f68fd2b1cad8a52282177
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-05 12:16:14 +0100

    Criação da classe ListaMembros

---
+++ src/migrantmatcher/domain/ListaMembros.java
@@ -0,0 +1,15 @@
+package migrantmatcher.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaMembros {
+
+	private List<Membro> lm = new ArrayList<>();
+
+	public void addOutroMembro(Membro membro) {
+		lm .add(membro);
+		
+	}
+	
+}
