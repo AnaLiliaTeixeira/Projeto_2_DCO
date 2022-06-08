4b59047e0777e0111d9c89a160c02786437a4397
Gacos11 <vascotbarros1@gmail.com>
2022-06-08 15:06:56 +0100

    Lista de Familias adicionada

--- src/migrantmatcher/domain/CatalogoMigrantes.java
+++ src/migrantmatcher/domain/CatalogoMigrantes.java
@@ -6,9 +6,13 @@ import java.util.List;
 public class CatalogoMigrantes {
 
 	private List<Migrante> catMigrantes = new ArrayList<>();
-	
+	private List<Familia> catFamilias = new ArrayList<>();
+		
 	public void addMigrante(Migrante m) {
 		catMigrantes.add(m);		
 	}
-
+	
+	public void addFamilia(Familia f) {
+		catFamilias.add(f);
+	}
 }
