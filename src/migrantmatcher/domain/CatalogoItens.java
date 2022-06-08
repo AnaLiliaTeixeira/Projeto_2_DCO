625cced0d237c80e2c92dfe6d78fa67570c6a7fd
Gacos11 <vascotbarros1@gmail.com>
2022-06-08 15:06:16 +0100

    Classe passa a extender da classe ListaAjudas

--- src/migrantmatcher/domain/CatalogoItens.java
+++ src/migrantmatcher/domain/CatalogoItens.java
@@ -3,9 +3,9 @@ package migrantmatcher.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CatalogoItens {
+public class CatalogoItens extends ListaAjudas{
 
-private List<Item> catItens = new ArrayList<>();
+	private List<Item> catItens = new ArrayList<>();
 	
 	public void addItem(String descricao) {
 		catItens.add(new Item(descricao));
