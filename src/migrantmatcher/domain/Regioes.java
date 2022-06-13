d8c0aae465698e0d7eed1bc30f27a9d3d01570d7
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-13 12:20:49 +0100

    Criação de um enumerado com as regiões possíveis

---
+++ src/migrantmatcher/domain/Regioes.java
@@ -0,0 +1,13 @@
+package migrantmatcher.domain;
+
+public enum Regioes {
+
+	NORTE,
+	ALGARVE,
+	CENTRO,
+	ÁREA_METROPOLITANA_DE_LISBOA,
+	ALENTEJO,
+	REGIÃO_AUTÓNOMA_DOS_AÇORES,
+	REGIÃO_AUTÓNOMA_DA_MADEIRA;
+	
+}
