7c03969161a677dcb6e290cd6e74cd1ce482bd1f
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-13 12:22:36 +0100

    ListaRegioes passa a obter regioes do enumerado

--- src/migrantmatcher/domain/ListaRegioes.java
+++ src/migrantmatcher/domain/ListaRegioes.java
@@ -6,14 +6,17 @@ import java.util.List;
 public class ListaRegioes {
 
 	private List<Regiao> lr = new ArrayList<>();
-	private String[] nutsII = {"NORTE", "ALGARVE", "CENTRO", "ÁREA_METROPOLITANA_DE_LISBOA",
-			"ALENTEJO", "REGIÃO_AUTÓNOMA_DOS_AÇORES", "REGIÃO_AUTÓNOMA_DA_MADEIRA"};
+//	private String[] nutsII = {"NORTE", "ALGARVE", "CENTRO", "ÁREA_METROPOLITANA_DE_LISBOA",
+//			"ALENTEJO", "REGIÃO_AUTÓNOMA_DOS_AÇORES", "REGIÃO_AUTÓNOMA_DA_MADEIRA"};
 	
+	public ListaRegioes() {
+		for (Regioes r : Regioes.values()) {
+			lr.add(setRegiao(r.toString()));
+			//lr.add(setRegiao(s));
+		}
+	}
 	
 	public List<Regiao> getListaRegioes() {
-		for (String s : nutsII) {
-			lr.add(setRegiao(s));
-		}
 		return lr;
 	}
 	
