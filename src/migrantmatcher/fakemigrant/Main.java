9bd1bb9a01753b948304efc4bbf8f4f23be9198a
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-13 12:23:45 +0100

    Valores são obtidos através do ficheiro properties

--- src/migrantmatcher/fakemigrant/Main.java
+++ src/migrantmatcher/fakemigrant/Main.java
@@ -1,46 +1,71 @@
 package migrantmatcher.fakemigrant;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.Random;
 import java.util.Scanner;
 
 import migrantmatcher.controllers.ProcuraAjudaHandler;
 import migrantmatcher.domain.Ajuda;
+import migrantmatcher.domain.Familia;
 import migrantmatcher.domain.ListaAjudas;
+import migrantmatcher.domain.Membro;
 import migrantmatcher.domain.Regiao;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		ProcuraAjudaHandler handler = new ProcuraAjudaHandler();
 		Random rd = new Random();
-		Scanner sc = new Scanner(System.in);
-		
-		if (rd.nextBoolean()) {
-			String nome = sc.nextLine();
-			int contacto = sc.nextInt();
-			handler.registaMigrante(nome, contacto);
+		//Scanner sc = new Scanner(System.in);
+		Properties prop = new Properties();
+
+
+		try {
+			prop.load(new FileInputStream(new File("prop_migrant.properties")));
+
+			if (rd.nextBoolean()) {
+				String nome = prop.getProperty("nomeMigrante");
+				int contacto = Integer.parseInt(prop.getProperty("contactoMigrante"));
+				handler.registaMigrante(nome, contacto);
+			}
+			else {
+				int numeroPessoas = Integer.parseInt(prop.getProperty("numeroPessoas"));
+				Familia f = new Familia(numeroPessoas);
+				String nomeCC = prop.getProperty("nomeCC");
+				int contactoCC = Integer.parseInt(prop.getProperty("contactoCC"));
+				handler.indicaDadosCasal(f, nomeCC, contactoCC);
+				String nomeOutroMembro;
+				
+				while((nomeOutroMembro = prop.getProperty("nomeOutroMembro")) != null) {
+					handler.indicaOutroMembro(f, nomeOutroMembro);
+					
+				} // duvida de como ler todas as linhas nomeOutroMembro do file properties
+				
+				handler.registaFamilia(f);
+			}
+
+			List<Regiao> lr = handler.pedeListaRegioesPossiveis();
+			String regiao = prop.getProperty("regiao");
+			ListaAjudas la = handler.indicaRegiaoEscolhida(regiao);
+			String ajuda = prop.getProperty("ajuda");
+			Ajuda a = handler.escolheAjuda(ajuda);
+			handler.confirmaPedidoAjuda();
+
+		} catch (FileNotFoundException e) {
+			//Do nothing
+			e.printStackTrace();
+		} catch (IOException e) {
+			//Do nothing
+			e.printStackTrace();
 		}
-		else {
-			int numeroPessoas = sc.nextInt();
-			handler.registaFamilia(numeroPessoas);
-			String nome = sc.nextLine();
-			int contacto = sc.nextInt();
-			handler.indicaDadosCasal(nome, contacto);
-			
-			do {
-				handler.indicaOutroMembro(nome);
-			} while (1==1/*migrante não deseja terminar*/);
-		}
-		
-		List<Regiao> lr = handler.pedeListaRegioesPossiveis();
-		String regiao = sc.nextLine();
-		ListaAjudas la = handler.indicaRegiaoEscolhida(regiao);
-		String ajuda = sc.nextLine();
-		Ajuda a = handler.escolheAjuda(ajuda);
-		handler.confirmaPedidoAjuda();
-		
+
 	}
 
 }
