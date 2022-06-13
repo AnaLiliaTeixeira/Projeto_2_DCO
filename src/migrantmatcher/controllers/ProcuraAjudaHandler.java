3b098880db7dfa46c780c755d4aca4d6bcd488e7
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-13 12:24:43 +0100

    Concretização de alguns métodos

--- src/migrantmatcher/controllers/ProcuraAjudaHandler.java
+++ src/migrantmatcher/controllers/ProcuraAjudaHandler.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import migrantmatcher.domain.Ajuda;
 import migrantmatcher.domain.CatalogoMigrantes;
+import migrantmatcher.domain.Familia;
 import migrantmatcher.domain.ListaAjudas;
 import migrantmatcher.domain.ListaRegioes;
 import migrantmatcher.domain.Membro;
@@ -14,24 +15,22 @@ public class ProcuraAjudaHandler {
 
 	private CatalogoMigrantes cm = new CatalogoMigrantes();
 	private ListaRegioes lr = new ListaRegioes();
-	//private ListaMembros lm = new ListaMembros();
 
 	public void registaMigrante(String nome, int contacto) {
 		cm.addMigrante(new Migrante(nome, contacto));		
 	}
 
-	public void registaFamilia(int numeroPessoas) {
-		// TODO Auto-generated method stub
-		
+	public void registaFamilia(Familia f) {	
+		cm.addFamilia(f);		
 	}
 	
-	public void indicaDadosCasal(String nome, int contacto) {
-		// TODO Auto-generated method stub
-		
+	public void indicaDadosCasal(Familia f, String nome, int contacto) {
+		f.setCC_nome(nome);
+		f.setCC_contacto(contacto);	
 	}
 	
-	public void indicaOutroMembro(String nome) {
-		//lm.addOutroMembro(new Membro(nome));	
+	public void indicaOutroMembro(Familia f, String nome) {
+		f.addMembro(new Membro(nome));
 	}
 
 	public List<Regiao> pedeListaRegioesPossiveis() {
@@ -40,12 +39,12 @@ public class ProcuraAjudaHandler {
 	}
 
 	public ListaAjudas indicaRegiaoEscolhida(String regiao) {
-		// TODO Auto-generated method stub
+		//temos que ver se existe na lr?
 		return null;
 	}
 	
 	public Ajuda escolheAjuda(String ajuda) {
-		// TODO Auto-generated method stub
+		//aqui será a mesma cena? NAO SEI??
 		return null;
 	}
 
