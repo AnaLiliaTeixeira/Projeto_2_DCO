749b24f7b5ec903116de75591a31affeda3e16a3
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-13 12:21:54 +0100

    Implementação do método addAjuda

--- src/migrantmatcher/domain/ListaAjudas.java
+++ src/migrantmatcher/domain/ListaAjudas.java
@@ -1,5 +1,13 @@
 package migrantmatcher.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaAjudas {
 
+	private List<Ajuda> la = new ArrayList<>();
+	
+	public void addAjuda(Ajuda a) {
+		la.add(a);
+	}
 }
