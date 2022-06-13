94139e87e73a36f27da08723a7f04f2a36dfacdb
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-13 23:52:42 +0100

    Criação do método getListaAjudasPossiveis

--- src/migrantmatcher/domain/ListaAjudas.java
+++ src/migrantmatcher/domain/ListaAjudas.java
@@ -6,8 +6,26 @@ import java.util.List;
 public class ListaAjudas {
 
 	private List<Ajuda> la = new ArrayList<>();
-	
+
 	public void addAjuda(Ajuda a) {
 		la.add(a);
+	}
+
+	public List<Ajuda> getListaAjudasPossiveis(Regiao regiao) {
+
+		List<Ajuda> listaAjudasPossiveis = new ArrayList<>();
+		for (Ajuda a : la) {
+			if (a.getClass().getSimpleName().equals("Alojamento")) {
+				if (!((Alojamento) a).getRegiao().equals(regiao)) {
+					//Do nothing
+				}
+			}
+			listaAjudasPossiveis.add(a);
+		}
+		return listaAjudasPossiveis;
+	}
+
+	public List<Ajuda> getListaAjudas() {
+		return la;
 	}
 }
