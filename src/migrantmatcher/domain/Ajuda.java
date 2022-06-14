8c0cb78b9d727b06d522289366b44248c4d2bfb0
Gacos11 <vascotbarros1@gmail.com>
2022-06-14 21:13:31 +0100

    Atualização do construtor da classe Ajuda

--- src/migrantmatcher/domain/Ajuda.java
+++ src/migrantmatcher/domain/Ajuda.java
@@ -8,6 +8,10 @@ public class Ajuda {
 		this.nome = ajuda;
 	}
 	
+	public Ajuda() {
+		//Do nothing
+	}
+	
 	public boolean equals(Object other) {
 		return this == other || (other instanceof Ajuda)
 				&& this.nome.equals(((Ajuda)other).nome);
