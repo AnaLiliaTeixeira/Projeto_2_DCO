ca91977f1bfab54a3b74158129ce411503f151d8
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-15 23:47:40 +0100

    Criação de getters e setters

--- src/migrantmatcher/domain/CatalogoAlojamentos.java
+++ src/migrantmatcher/domain/CatalogoAlojamentos.java
@@ -1,16 +1,17 @@
 package migrantmatcher.domain;
 
-import java.util.HashMap;
 import java.util.List;
 
 public class CatalogoAlojamentos extends ListaAjudas{
 
-	private HashMap<Voluntario, List<Alojamento>> catAloj;
+	private List<Alojamento> catAloj;
 	
-	public void addAlojamento(int numeroPessoas, Regiao regiao, Voluntario vol) {
-		List<Alojamento> newList = catAloj.get(vol);
-		newList.add(new Alojamento(numeroPessoas, regiao));
-		catAloj.put(vol, newList);
+	public void addAlojamento(Alojamento aloj) {
+		catAloj.add(aloj);
+	}
+	
+	public List<Alojamento> getCatalogoAlojamentos() {
+		return catAloj;
 	}
 	
 }
