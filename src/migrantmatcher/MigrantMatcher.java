bea18ec133e691019e30be7bc422f1f059cb237d
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-15 23:53:32 +0100

    ProcuraAjudaHandler adicionado

--- src/migrantmatcher/MigrantMatcher.java
+++ src/migrantmatcher/MigrantMatcher.java
@@ -1,13 +1,29 @@
 package migrantmatcher;
 
+import migrantmatcher.controllers.ProcuraAjudaHandler;
 import migrantmatcher.controllers.RegistaAjudaHandler;
+import migrantmatcher.domain.CatalogoAlojamentos;
+import migrantmatcher.domain.CatalogoItens;
+import migrantmatcher.domain.CatalogoMigrantes;
 import migrantmatcher.domain.CatalogoVoluntarios;
 
 public class MigrantMatcher {
 
-	private CatalogoVoluntarios catVoluntario = new CatalogoVoluntarios();
+	private CatalogoVoluntarios catVoluntarios = new CatalogoVoluntarios();
+	private CatalogoItens catItens = new CatalogoItens();
+	private CatalogoAlojamentos catAloj = new CatalogoAlojamentos();
+	private CatalogoMigrantes catMigrantes = new CatalogoMigrantes();
 
 	public RegistaAjudaHandler getRegistaAjudaHandler() {
-		return new RegistaAjudaHandler(catVoluntario);
+		return new RegistaAjudaHandler(catVoluntarios, catItens, catAloj);
 	}
+	
+	public ProcuraAjudaHandler getProcuraAjudaHandler() {
+		return new ProcuraAjudaHandler(catMigrantes , catItens, catAloj);
+	}
+
+	public CatalogoItens getCatalogoItens() {
+		return catItens;
+	}
+	
 }
