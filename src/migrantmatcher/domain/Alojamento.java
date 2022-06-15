decaac1aa380a7aba86493254c0cbc4d56411f55
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-15 23:47:24 +0100

    Criação de getters e setters

--- src/migrantmatcher/domain/Alojamento.java
+++ src/migrantmatcher/domain/Alojamento.java
@@ -1,17 +1,37 @@
 package migrantmatcher.domain;
 
+import java.time.LocalDateTime;
+
 public class Alojamento extends Ajuda {
 
 	private Regiao regiao;
 	private int numeroPessoas;
-		
+	private Voluntario voluntario;	
+	private LocalDateTime createdTime;
 	
-	public Alojamento(int numeroPessoas, Regiao regiao) {
+	public Alojamento(int numeroPessoas, Voluntario voluntario) {
 		this.numeroPessoas = numeroPessoas;
-		this.regiao = regiao; //como criamos outro construtor sem ser o do super?
+		this.voluntario = voluntario;
+		this.createdTime = LocalDateTime.now();
 	}
 
 	public Regiao getRegiao() {
 		return regiao;
+	}
+
+	public int getNumeroPessoas() {
+		return numeroPessoas;
+	}
+	
+	public Voluntario getVoluntario() {
+		return voluntario;
+	}
+
+	public LocalDateTime getCreatedTime() {
+		return createdTime;
+	}
+	
+	public void setRegiao(Regiao regiao) {
+		this.regiao = regiao;
 	}
 }
