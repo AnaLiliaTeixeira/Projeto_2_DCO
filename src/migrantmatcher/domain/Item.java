50f22689633a01ee56d69b9b14e650706f0d71d0
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-15 23:51:36 +0100

    Criação de getters e atributos adicionados

--- src/migrantmatcher/domain/Item.java
+++ src/migrantmatcher/domain/Item.java
@@ -1,14 +1,28 @@
 package migrantmatcher.domain;
 
+import java.time.LocalDateTime;
+
 public class Item extends Ajuda{
 
 	private String descricao;
+	private Voluntario voluntario;
+	private LocalDateTime createdTime;
 	
-	public Item(String descricao) {
+	public Item(String descricao, Voluntario voluntario) {
 		this.descricao = descricao;
+		this.voluntario = voluntario;
+		this.createdTime = LocalDateTime.now();
 	}
 
 	public String getDescricao() {
 		return descricao;
+	}
+	
+	public Voluntario getVoluntario() {
+		return voluntario;
+	}
+
+	public LocalDateTime getCreatedTime() {
+		return createdTime;
 	}
 }
