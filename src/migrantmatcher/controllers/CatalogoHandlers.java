8a35aa965584619a763e2a5276b8de45661635e3
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2022-06-16 22:51:49 +0100

    Criação do método addHandler e atributo catHandlers

--- src/migrantmatcher/controllers/CatalogoHandlers.java
+++ src/migrantmatcher/controllers/CatalogoHandlers.java
@@ -1,5 +1,13 @@
 package migrantmatcher.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CatalogoHandlers {
 
+	List<Handler> catHandlers = new ArrayList<>();
+	
+	public void addHandler(Handler h) {
+		catHandlers.add(h);
+	}
 }
